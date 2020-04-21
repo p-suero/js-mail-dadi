aee8d16c2f88468c891bacdc76ea16194667f9ca
Paolo <paolo.suero@gmail.com>
2020-04-21 18:08:35 +0200

    Risolto risultato plurimo mail

--- mail/main.js
+++ mail/main.js
@@ -1,16 +1,25 @@
 // chiedo all'utente di inserire la sua emaio
 var email_utente = prompt("Inserisci la tua email");
 console.log(email_utente);
+
 //creo l'array con la lista delle email per creare un database
 var email_database = ["paolo.suero@outlook.it","mario.rossi@ciao.it", "danilo.inverno@due.com", "maria.ferrari@libero.it"];
 console.log(email_database);
+
+//creo la variabile "emailInLista per estrapolare il risultato dal ciclo"
+var emailInList
 
 //creo il ciclo per accedere alla sequenza dell array
 for (var i = 0; i < email_database.length; i++) {
     console.log(email_database[i]);
     if (email_utente == (email_database[i])) {
-        console.log('Perfetto sei in lista');
-    } else {
-        console.log("Ci dispiace non sei in lista");
-    }
+        emailInList = true;
+    } 
+}
+
+// stampo il risultato in pagina HTML
+if (emailInList == true) {
+    document.writeln('Complimenti, sei in lista');
+} else {
+    alert('Ci dispiace non sei in lista. Inserisci un email corretta!');
 }
